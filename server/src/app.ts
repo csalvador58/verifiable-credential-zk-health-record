cf32b4f2e0c4b48661df0a4dc86627eeb5bb612b
Chris Salvador <csalvador58@gmail.com>
2023-09-21 22:04:16 -0700

    add ts server

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response, NextFunction, Express } from 'express';
+import routes from './routes/index';
+
+const app: Express = express();
+app.use(express.json());
+
+app.use(routes);
+
+app.get('/', (_req: Request, res: Response, _next: NextFunction) => {
+  res.send(`
+        <html>
+            <body>
+                <h1>Server deployed!</h1>
+            </body>
+        </html>
+    `);
+});
