8c96082a62a580a02bcc8424b636f6c1e68f7fd2
Chris Salvador <csalvador58@gmail.com>
2023-09-23 02:48:39 -0700

    Add hide option on sign-in option of did section

--- med-app/src/pages/did/index.tsx
+++ med-app/src/pages/did/index.tsx
@@ -1,19 +1,38 @@
 import { Container, Group } from '@mantine/core';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Loading } from '../../components/Loading';
 import { SideMenu } from '../../components/SideMenu';
 
-export const sideMenu = {
-  title: 'Menu',
-  menu: [
-    { name: 'Sign-In', href: '/did-main/login' },
-    { name: 'View List', href: '/did-main/DID-items' },
-    { name: 'Create', href: '/did-main/create' },
-  ],
-};
+// export const sideMenu = {
+//   title: 'Menu',
+//   menu: [
+//     { name: 'Sign-In', href: '/did-main/login' },
+//     { name: 'View List', href: '/did-main/DID-items' },
+//     { name: 'Create', href: '/did-main/create' },
+//   ],
+// };
 
 export function DIDMainPage(): JSX.Element {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const sideMenu = !isLoggedIn ? 
+    {
+      title: 'Menu',
+      menu: [
+        { name: 'Sign-In', href: '/did-main/login' },
+        { name: 'View List', href: '/did-main/DID-items' },
+        { name: 'Create', href: '/did-main/create' },
+      ],
+    } :
+    {
+      title: 'Menu',
+      menu: [
+        { name: 'View List', href: '/did-main/DID-items' },
+        { name: 'Create', href: '/did-main/create' },
+      ],
+    };
+
   return (
     <Container>
       <Group align="top">
