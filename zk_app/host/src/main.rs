c45acd88c913b8897f0429a857268cd94dc1f316
Chris Salvador <csalvador58@gmail.com>
2023-09-25 23:45:29 -0700

    setup crates

--- zk_app/host/src/main.rs
+++ zk_app/host/src/main.rs
@@ -1,7 +1,4 @@
-// TODO: Update the name of the method loaded by the prover. E.g., if the method
-// is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
-// `METHOD_NAME_ID` with `MULTIPLY_ID`
-use methods::{METHOD_NAME_ELF, METHOD_NAME_ID};
+use zk_methods::{VALIDATE_ELF, VALIDATE_ID};
 use risc0_zkvm::{default_prover, ExecutorEnv};
 
 fn main() {
@@ -21,12 +18,12 @@ fn main() {
     let prover = default_prover();
 
     // Produce a receipt by proving the specified ELF binary.
-    let receipt = prover.prove_elf(env, METHOD_NAME_ELF).unwrap();
+    let receipt = prover.prove_elf(env, VALIDATE_ELF).unwrap();
 
     // TODO: Implement code for transmitting or serializing the receipt for
     // other parties to verify here
 
     // Optional: Verify receipt to confirm that recipients will also be able to
     // verify your receipt
-    receipt.verify(METHOD_NAME_ID).unwrap();
+    receipt.verify(VALIDATE_ID).unwrap();
 }
